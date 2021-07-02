0dbc106cc4c4c2b6a401179266514e9c5f150ad5
Trent Gill <trent.gill@gmail.com>
2021-07-03 05:52:59 +0700

    modernize

--- lib/crow.lua
+++ lib/crow.lua
@@ -1,12 +1,13 @@
 _crow = {}
 
 function _crow.init()
-  crow.init()
-  crow.clear()
-  crow.reset()
   crow.output[2].action = "pulse(.025, 5, 1)"
   crow.output[4].action = "pulse(.025, 5, 1)"
-  crow.ii.pullup(true)
+  crow.ii.jf.mode(1)
+end
+
+norns.crow.add = function()
+  norns.crow.init()
   crow.ii.jf.mode(1)
 end
 
@@ -17,7 +18,7 @@ end
 function _crow:play(note, pair)
   local output_pairs = {{1,2},{3,4}}
   crow.output[output_pairs[pair][1]].volts = (note - 60) / 12
-  crow.output[output_pairs[pair][2]].execute()
+  crow.output[output_pairs[pair][2]]()
 end
 
 return _crow
