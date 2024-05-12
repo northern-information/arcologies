c92882fc1c346b0638701f13878e9b1154887c63
Cameron Entzminger <centzminger1@gmail.com>
2024-05-12 22:44:29 +0100

    initASL added

--- lib/_crow.lua
+++ lib/_crow.lua
@@ -1,8 +1,7 @@
 _crow = {}
 
 function _crow.init()
-  crow.output[2].action = "pulse(.025, 5)"
-  crow.output[4].action = "pulse(.025, 5)"
+  initASL()
   crow.ii.jf.mode(1)
 end
 
@@ -16,9 +15,21 @@ function _crow:jf(note)
 end
 
 function _crow:play(note, pair)
+  initASL()
   local output_pairs = {{1,2},{3,4}}
   crow.output[output_pairs[pair][1]].volts = (note - 60) / 12
+  -- setting the action before triggering fixes the problem where 
+  -- power cycling crow results in default ASL after arcologies has already initialized, better way?
+
   crow.output[output_pairs[pair][2]]()
+  -- power cycle crow sets default ASL loaded on outputs, that's why only note is working
+  -- action is triggering on 2, there's just nothing there
+  -- reload script, it initializes crow output ASL actions
+end
+
+function initASL()
+  crow.output[2].action = "pulse(.025, 5)"
+  crow.output[4].action = "pulse(.025, 5)"
 end
 
 return _crow
