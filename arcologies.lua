eadf8b3b704ac64da1894056d35884ca1dfa1ecb
Tyler Etters <tyler@etters.co>
2024-06-30 10:45:33 -0700

    --

--- arcologies.lua
+++ arcologies.lua
@@ -42,7 +42,7 @@ function init()
   menu.init()
   popup.init()
   keeper.init()
-  api.init() 
+  api.init()
   arcology_name = "arcology" .. os.time(os.date("!*t"))
   grid_dirty, screen_dirty, splash_break, arcology_loaded = false, false, false, false
   keys, key_counter, enc_counter = {}, {{},{},{}}, {{},{},{}}
